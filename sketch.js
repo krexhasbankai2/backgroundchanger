8d6a8c32711bc1fb063854a6aec0edbe4e62cfd9
krexhasbankai2 <87955280+krexhasbankai2@users.noreply.github.com>
2022-03-17 21:22:11 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,30,30)
+  background(51)
+}
+
+function draw() 
+{
+  background(30);
+
+if(keyIsDown(RIGHT_ARROW)){
+background("red")
+}
+if(keyIsDown(LEFT_ARROW)){
+  background("blue")
+  }
+  if(keyIsDown(UP_ARROW)){
+    background("yellow")
+    }
+    if(keyIsDown(DOWN_ARROW)){
+      background("green")
+      }
+
+
+
+drawSprites();
+}
+
+
+
+
